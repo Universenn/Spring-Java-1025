28e0d84dba8224e65bc19a258913d6304f4e3b24
Universenn <dnwndhks98@gmail.com>
2022-10-25 16:06:04 +0900

    Spring JdbcTemplate적용

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -6,140 +6,54 @@ import com.statementstrategy.AddStrategy;
 import com.statementstrategy.DeleteAllStrategy;
 import com.statementstrategy.StatementStrategy;
 import com.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-//    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    private final DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     private Connection c;
     private PreparedStatement ps;
 
-//    public UserDao(ConnectionMaker connectionMaker) {
-//        this.connectionMaker = connectionMaker;
-//    }
-
-    public void jdbcContextStatementStrategy(StatementStrategy stmst){
-        c = null;
-        ps = null;
-        try {
-            c = dataSource.getConnection();
-            ps = stmst.getStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-        }
+    public void deleteAll(){
+//        this.dataSource = dataSource;
+        this.jdbcTemplate.update("delete from users");
     }
-    public void deleteAll() throws SQLException, ClassNotFoundException {
-//        jdbcContextStatementStrategy(new DeleteAllStrategy());
-        jdbcContextStatementStrategy(new StatementStrategy() {
+
+    public void add(User user){
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",user.getId(),user.getName(),user.getPassword());
+
+    }
+
+    public User findById(String id){
+        String sql = "SELECT * FROM users WHERE id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
             @Override
-            public PreparedStatement getStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+                return user;
             }
-        });
+        };
+        return jdbcTemplate.queryForObject(sql,rowMapper,id);
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-//        jdbcContextStatementStrategy(new AddStrategy(user));
-        jdbcContextStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement getStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
-            }
-        });
+
+    public int getCount(){
+        String sql = "SELECT count(*) FROM users";
+        return jdbcTemplate.queryForObject(sql,Integer.class);
     }
 
-    public User findById(String id) throws ClassNotFoundException, SQLException {
-        c = dataSource.getConnection();
-//        c = connectionMaker.makeConnection();
 
-        ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-        if (rs.next()) {
-            user = new User(
-                    rs.getString("id"),
-                    rs.getString("name"),
-                    rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return user;
-    }
-
-    public int getCount() throws SQLException, ClassNotFoundException {
-        c = null;
-        ps = null;
-        ResultSet rs = null;
-        try {
-            c = dataSource.getConnection();
-//            c = connectionMaker.makeConnection();
-            ps = c.prepareStatement("SELECT count(*) FROM users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-        }
-//        return count;
-    }
-
-//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        UserDao userDao = new UserDao(new AwsConnectionMaker());
-//        User user = new User("id", "name", "password");
-//        userDao.add(user);
-//        user = userDao.findById("id");
-//        System.out.println(user.getName());
-//    }
 }
